bf5b78ab7f9090b3e54eb0087b7526b5892decea
Oscarz90 <omsz90@live.com.mx>
2013-04-17 17:42:13 -0500

    Ya stan todas las clases de la Capa de Negocios (ORM)

---
+++ CapaNegocios/CapaNegocios/ClasesIndividuales/Clasificacion_Modelo.vb
@@ -0,0 +1,38 @@
+﻿Imports CapaDatos
+Public Class Clasificacion_Modelo
+    Implements IIndividual
+    Dim cadena_conexion As New CapaDatos.conexiones
+    Dim oBD As New CapaDatos.CapaDatos(cadena_conexion.CadenaSicaip)
+#Region "IIndividual"
+    Public Sub Cargar() Implements IIndividual.Cargar
+
+    End Sub
+
+    Public Sub Eliminar() Implements IIndividual.Eliminar
+
+    End Sub
+    Dim vId As Long
+    Public Property Id As Long Implements IIndividual.Id
+        Get
+            Return vId
+        End Get
+        Set(ByVal value As Long)
+            vId = value
+        End Set
+    End Property
+
+    Public Function Obtener_Id(ByVal vCadena As String) As Long Implements IIndividual.Obtener_Id
+        Return 1
+    End Function
+
+    Public Sub Registrar() Implements IIndividual.Registrar
+
+    End Sub
+#End Region
+#Region "Atributos"
+
+#End Region
+#Region "Propiedades"
+
+#End Region
+End Class
