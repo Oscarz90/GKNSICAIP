ee6cd64a519d26a0c3b7ae69a548c4e4262c8d27
paty <clau_pms89@hotmail.com>
2013-04-18 01:08:21 -0500

    guardado

--- CapaNegocios/CapaNegocios/ClasesIndividuales/Cadena_Valor.vb
+++ CapaNegocios/CapaNegocios/ClasesIndividuales/Cadena_Valor.vb
@@ -3,36 +3,80 @@ Public Class Cadena_Valor
     Implements IIndividual
     Dim cadena_conexion As New CapaDatos.conexiones
     Dim oBD As New CapaDatos.CapaDatos(cadena_conexion.CadenaSicaip)
+
 #Region "IIndividual"
     Public Sub Cargar() Implements IIndividual.Cargar
-
+        Dim vDR As DataRow
+        vDR = oBD.ObtenerRenglon("select * from cadena_valor where cve_cadena_valor = " & vCve_cadena_valor, "cadena_valor")
+        If vDR IsNot Nothing Then
+            vCve_cadena_valor = vDR("cve_cadena_valor")
+            vCadena = vDR("cadena")
+        End If
     End Sub
 
     Public Sub Eliminar() Implements IIndividual.Eliminar
+        Try
+            oBD.EjecutarQuery("delete from cadena_valor where cve_cadena_valor = " & vCve_cadena_valor)
+        Catch ex As Exception
 
+        End Try
     End Sub
-    Dim vId As Long
-    Public Property Id As Long Implements IIndividual.Id
-        Get
-            Return vId
-        End Get
-        Set(ByVal value As Long)
-            vId = value
-        End Set
-    End Property
 
     Public Function Obtener_Id(ByVal vCadena As String) As Long Implements IIndividual.Obtener_Id
-        Return 1
+        Dim vDR As DataRow
+        Dim vRetorno As Long
+        vDR = oBD.ObtenerRenglon("select cve_cadena_valor from cadena_valor where cadena = " & vCadena, "cadena_valor")
+        If vDR IsNot Nothing Then
+            vRetorno = vDR("cve_cadena_valor")
+        Else
+            vRetorno = 0
+        End If
+        Return vRetorno
     End Function
 
     Public Sub Registrar() Implements IIndividual.Registrar
 
+        Using scope As New TransactionScope
+            Try
+                Dim cmd As New SqlClient.SqlCommand
+                cmd.CommandType = CommandType.StoredProcedure
+                cmd.CommandText = "REGISTRAR_cadena_valor"
+                cmd.Parameters.Add("@cve_cadena_valor", SqlDbType.Int).Value = Me.vCve_cadena_valor
+                cmd.Parameters.Add("@cadena", SqlDbType.VarChar).Value = Me.vCadena
+
+                Dim obj As DataTable = oBD.EjecutaCommando(cmd)
+                Me.vCve_cadena_valor = obj.Rows(0)(0)
+                scope.Complete()
+            Catch ex As Exception
+            End Try
+        End Using
     End Sub
 #End Region
-#Region "Atributos"
 
+#Region "Atributos"
+    Private vCve_cadena_valor As Long
+    Private vCadena As String
 #End Region
 #Region "Propiedades"
+
+    Public Property Cve_cadena_valor() As Long Implements IIndividual.Id
+        Get
+            Return vCve_cadena_valor
+        End Get
+        Set(ByVal value As Long)
+            vCve_cadena_valor = value
+        End Set
+    End Property
+
+
+    Public Property Cadena() As String
+        Get
+            Return vCadena
+        End Get
+        Set(ByVal value As String)
+            vCadena = value
+        End Set
+    End Property
 
 #End Region
 End Class
--- CapaNegocios/CapaNegocios/ClasesIndividuales/Clasificacion_Modelo.vb
+++ CapaNegocios/CapaNegocios/ClasesIndividuales/Clasificacion_Modelo.vb
@@ -5,34 +5,80 @@ Public Class Clasificacion_Modelo
     Dim oBD As New CapaDatos.CapaDatos(cadena_conexion.CadenaSicaip)
 #Region "IIndividual"
     Public Sub Cargar() Implements IIndividual.Cargar
-
+        Dim vDR As DataRow
+        vDR = oBD.ObtenerRenglon("select * from clasificacion_modelo where cve_clasificacion_modelo =" & vCve_clasificacion_modelo, "clasificacion_modelo")
+        If vDR IsNot Nothing Then
+            vCve_clasificacion_modelo = vDR("cve_clasificacion_modelo")
+            vDescripcion = vDR("descripcion")
+        End If
     End Sub
 
     Public Sub Eliminar() Implements IIndividual.Eliminar
+        Try
+            oBD.EjecutarQuery("delete from clasificacion_modelo where cve_clasificacion_modelo =" & vCve_clasificacion_modelo)
+        Catch ex As Exception
 
+        End Try
     End Sub
-    Dim vId As Long
-    Public Property Id As Long Implements IIndividual.Id
-        Get
-            Return vId
-        End Get
-        Set(ByVal value As Long)
-            vId = value
-        End Set
-    End Property
 
     Public Function Obtener_Id(ByVal vCadena As String) As Long Implements IIndividual.Obtener_Id
-        Return 1
+        Dim vDR As DataRow
+        Dim vRetorno As Long
+        vDR = oBD.ObtenerRenglon("select cve_clasificacion_modelo from clasificacion_modelo where descripcion= " & vCadena, "clasificacion_modelo")
+        If vDR IsNot Nothing Then
+            vRetorno = vDR("cve_clasificacion_modelo")
+        Else
+            Return 0
+        End If
+        Return vRetorno
     End Function
 
     Public Sub Registrar() Implements IIndividual.Registrar
 
+        Using scope As New TransactionScope
+            Try
+                Dim cmd As New SqlClient.SqlCommand
+                cmd.CommandType = CommandType.StoredProcedure
+                cmd.CommandText = "REGISTRAR_clasificacion_modelo"
+                cmd.Parameters.Add("@cve_clasificacion_modelo", SqlDbType.Int).Value = Me.vCve_clasificacion_modelo
+                cmd.Parameters.Add("@descripcion", SqlDbType.VarChar).Value = Me.vCve_clasificacion_modelo
+
+                Dim obj As DataTable = oBD.EjecutaCommando(cmd)
+                Me.vCve_clasificacion_modelo = obj.Rows(0)(0)
+                scope.Complete()
+
+            Catch ex As Exception
+
+            End Try
+        End Using
     End Sub
-#End Region
-#Region "Atributos"
 
 #End Region
+
+#Region "Atributos"
+    Private vCve_clasificacion_modelo As Long
+    Private vDescripcion As String
+#End Region
 #Region "Propiedades"
+
+    Public Property Cve_clasificacion_modelo() As Long Implements IIndividual.Id
+        Get
+            Return vCve_clasificacion_modelo
+        End Get
+        Set(ByVal value As Long)
+            vCve_clasificacion_modelo = value
+        End Set
+    End Property
+
+    Public Property Descripcion() As String
+        Get
+            Return vDescripcion
+        End Get
+        Set(ByVal value As String)
+            vDescripcion = value
+        End Set
+    End Property
+
 
 #End Region
 End Class
