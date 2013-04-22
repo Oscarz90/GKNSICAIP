da17cd83c141db4d508bb6f40e12b0ff4168bbc1
Oscarz90 <omsz90@live.com.mx>
2013-04-22 15:56:32 -0500

    save agregue comentarios entre regiones

--- GKNSICAIP/GKNSICAIP/Formularios/Procesos/Maquinado/frmProduccion.vb
+++ GKNSICAIP/GKNSICAIP/Formularios/Procesos/Maquinado/frmProduccion.vb
@@ -30,6 +30,7 @@ Public Class frmProduccion
     End Sub
 #End Region
 #Region "Inicializando formulario"
+    'Establece variables globales
     Public Sub set_Datos_Equipo(ByVal idEq As Integer, ByVal NomEquipo As String, ByVal Empleado As String, ByVal CodEmpleado As String)
         vcve_equipo = idEq
         vnombre_equipo = NomEquipo
@@ -38,7 +39,7 @@ Public Class frmProduccion
         lblNombreEquipo.Text = vnombre_equipo
 
     End Sub
-
+    'Load
     Private Sub frmProduccion_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         llena_cbx_Turnos()
         llena_cbx_Lineas()
@@ -49,6 +50,7 @@ Public Class frmProduccion
 
 #End Region
 #Region "LLenado ComboBoxs"
+    'Turnos
     Private Sub llena_cbx_Turnos()
         Dim oTurnos As New CapaNegocios.Turno
         cbxTurno.ValueMember = "cve_turno"
@@ -56,6 +58,7 @@ Public Class frmProduccion
         cbxTurno.DataSource = oTurnos.llena_combo_turnos
         cbxTurno.SelectedIndex = -1
     End Sub
+    'Lineas
     Private Sub llena_cbx_Lineas()
         Dim oEquipoLinea As New CapaNegocios.EquipoLinea
         oEquipoLinea.cve_equipo = vcve_equipo
@@ -65,6 +68,7 @@ Public Class frmProduccion
         cbxLinea.SelectedIndex = -1
         flgBanderacbxLineas = True
     End Sub
+    'Modelos (por Linea)
     Private Sub llena_cbx_Modelos()
         flgBanderacbxModelos = False
         If cbxLinea.SelectedIndex <> -1 And flgBanderacbxLineas Then
@@ -72,16 +76,27 @@ Public Class frmProduccion
             cbxModeloProductividad.ValueMember = "cve_modelo"
             cbxModeloProductividad.DisplayMember = "np_gkn"
             cbxModeloProductividad.DataSource = oModelo.llena_combo_Modelos_Linea(cbxLinea.SelectedValue)
+            cbxModeloProductividad.SelectedIndex = -1
+
             cbxModeloDesecho.ValueMember = "cve_modelo"
             cbxModeloDesecho.DisplayMember = "np_gkn"
             cbxModeloDesecho.DataSource = oModelo.llena_combo_Modelos_Linea(cbxLinea.SelectedValue)
-            cbxModeloProductividad.SelectedIndex = -1
             cbxModeloDesecho.SelectedIndex = -1
+
+
+            cbxModeloRechazo.ValueMember = "cve_modelo"
+            cbxModeloRechazo.DisplayMember = "np_gkn"
+            cbxModeloRechazo.DataSource = oModelo.llena_combo_Modelos_Linea(cbxLinea.SelectedValue)
+            cbxModeloRechazo.SelectedIndex = -1
+
+
+
             flgBanderacbxModelos = True
         End If
     End Sub
 #End Region
 #Region "Llenado Labels - Textbox"
+    'General
     Private Sub obten_descripcion_modelo(ByRef txtmodelodesc As TextBox, ByRef cbxmodelodesc As ComboBox)
         If flgBanderacbxModelos Then
             Dim oModelo As New Modelo
@@ -92,11 +107,13 @@ Public Class frmProduccion
     End Sub
 #End Region
 #Region "Llenado Gridsviews"
+    'Productividad
     Private Sub llena_productividad_gridview()
         Dim oProduccion As New Produccion
         oProduccion.cve_registro_turno = 1
         grdDetalleProductividad.DataSource = oProduccion.llena_productividad_gridview()
     End Sub
+    'Desechos
     Private Sub llena_desecho_gridview()
         Dim oDesecho As New Desecho
         oDesecho.cve_registro_turno = 1
@@ -104,34 +121,45 @@ Public Class frmProduccion
     End Sub
 #End Region
 #Region "Eventos ComboBox"
+    'General
     Private Sub cbxLinea_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbxLinea.SelectedIndexChanged
         llena_cbx_Modelos()
     End Sub
+    'Productividad
     Private Sub cbxModeloProductividad_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbxModeloProductividad.SelectedIndexChanged
         If cbxModeloProductividad.SelectedIndex <> -1 Then
             obten_descripcion_modelo(txtModeloDescripcionProductividad, cbxModeloProductividad)
-            deshabilitar_btn_Quitar_modelo()
+            deshabilitar_btn_quitar_modelo()
         End If
     End Sub
-    Private Sub cbxModeloRechazo_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbxModeloDesecho.SelectedIndexChanged
+    'Desechos
+    Private Sub cbxModeloDesecho_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbxModeloDesecho.SelectedIndexChanged
         If cbxModeloDesecho.SelectedIndex <> -1 Then
             obten_descripcion_modelo(txtModeloDesecho, cbxModeloDesecho)
             deshabilitar_btn_quitar_desecho()
         End If
-
+    End Sub
+    'Rechazos
+    Private Sub cbxModeloRechazo_SelectedIndexChanged_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbxModeloRechazo.SelectedIndexChanged
+        If cbxModeloRechazo.SelectedIndex <> -1 Then
+            obten_descripcion_modelo(txtModeloRechazo, cbxModeloRechazo)
+            deshabilitar_btn_quitar_rechazo()
+        End If
     End Sub
 #End Region
 #Region "Eventos TextBox"
+    'Productividad
     Private Sub txtMinutos_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtTiempoOperacion.TextChanged
         valida_sea_numero(sender)
         valida_botones_productividad()
-        deshabilitar_btn_Quitar_modelo()
+        deshabilitar_btn_quitar_modelo()
     End Sub
     Private Sub txtPiezasProducidas_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtPiezasOkProducidas.TextChanged
         valida_sea_numero(sender)
         valida_botones_productividad()
-        deshabilitar_btn_Quitar_modelo()
+        deshabilitar_btn_quitar_modelo()
     End Sub
+    'Desechos
     Private Sub txtDesechosCantidad_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtDesechosCantidad.TextChanged
         valida_sea_numero(sender)
         valida_botones_desecho()
@@ -139,6 +167,7 @@ Public Class frmProduccion
     End Sub
 #End Region
 #Region "Eventos Botones"
+    'Productividad
     Private Sub btnAgregarModelo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAgregarModelo.Click
         add_modelo_producido()
         limpia_productividad()
@@ -147,8 +176,9 @@ Public Class frmProduccion
     Private Sub btnQuitarModelo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnQuitarModelo.Click
         remove_modelo_producido()
         llena_productividad_gridview()
-        deshabilitar_btn_Quitar_modelo()
+        deshabilitar_btn_quitar_modelo()
     End Sub
+    'Desechos
     Private Sub btnAgregarDesecho_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAgregarDesecho.Click
         add_desecho()
         limpia_desechos()
@@ -161,15 +191,24 @@ Public Class frmProduccion
     End Sub
 #End Region
 #Region "Eventos Gridviews"
+    'Productividad
     Private Sub grdDetalleProductividad_CellClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles grdDetalleProductividad.CellClick
         'Dim cve_produccion As Long = grdDetalleProductividad.Item("colcve_produccion", grdDetalleProductividad.CurrentRow.Index).Value
         habilita_btn_Quitar_modelo()
     End Sub
+    'Desechos
     Private Sub grdDetalleDesecho_CellClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles grdDetalleDesecho.CellClick
         habilita_btn_Quitar_desecho()
     End Sub
 #End Region
 #Region "Validaciones"
+    'Generales
+    Private Sub valida_sea_numero(ByRef cajaDeTexto As TextBox)
+        If cajaDeTexto.Text <> "" And Not IsNumeric(cajaDeTexto.Text) Then
+            cajaDeTexto.Text = "0"
+        End If
+    End Sub
+    'Productividad
     Private Sub valida_botones_productividad()
         If cbxModeloProductividad.SelectedIndex <> -1 And txtTiempoOperacion.Text <> "" And txtPiezasOkProducidas.Text <> "" And txtPiezasOkProducidas.Text <> "0" And txtTiempoOperacion.Text <> "0" Then
             If cbxTurno.SelectedIndex <> -1 Then
@@ -179,6 +218,14 @@ Public Class frmProduccion
             btnAgregarModelo.Enabled = False
         End If
     End Sub
+    Private Sub habilita_btn_Quitar_modelo()
+        limpia_productividad()
+        btnQuitarModelo.Enabled = True
+    End Sub
+    Private Sub deshabilitar_btn_quitar_modelo()
+        btnQuitarModelo.Enabled = False
+    End Sub
+    'Rechazo
     Private Sub valida_botones_desecho()
         If cbxModeloDesecho.SelectedIndex <> -1 And txtDesechosCantidad.Text <> "" And txtDesechosCantidad.Text <> "0" Then
             If cbxTurno.SelectedIndex <> -1 Then
@@ -188,17 +235,22 @@ Public Class frmProduccion
             btnAgregarDesecho.Enabled = False
         End If
     End Sub
-    Private Sub valida_sea_numero(ByRef cajaDeTexto As TextBox)
-        If cajaDeTexto.Text <> "" And Not IsNumeric(cajaDeTexto.Text) Then
-            cajaDeTexto.Text = "0"
+    Private Sub habilita_btn_Quitar_rechazo()
+        limpia_rechazos()
+        btnQuitarRechazo.Enabled = True
+    End Sub
+    Private Sub deshabilitar_btn_quitar_rechazo()
+        btnQuitarRechazo.Enabled = False
+    End Sub
+    'Desecho
+    Private Sub valida_botones_rechazos()
+        If cbxModeloRechazo.SelectedIndex <> -1 And txtRechazosCantidad.Text <> "" And txtRechazosCantidad.Text <> "0" And cbxTipoRechazo.SelectedIndex <> -1 Then
+            If cbxTurno.SelectedIndex <> -1 Then
+                btnAgregarRechazo.Enabled = True
+            End If
+        Else
+            btnAgregarRechazo.Enabled = False
         End If
-    End Sub
-    Private Sub habilita_btn_Quitar_modelo()
-        limpia_productividad()
-        btnQuitarModelo.Enabled = True
-    End Sub
-    Private Sub deshabilitar_btn_quitar_modelo()
-        btnQuitarModelo.Enabled = False
     End Sub
     Private Sub habilita_btn_Quitar_desecho()
         limpia_desechos()
@@ -209,19 +261,29 @@ Public Class frmProduccion
     End Sub
 #End Region
 #Region "Limpia formularios"
+    'Productividad
     Private Sub limpia_productividad()
         cbxModeloProductividad.SelectedIndex = -1
         txtPiezasOkProducidas.Text = "0"
         txtTiempoOperacion.Text = ""
         txtModeloDescripcionProductividad.Text = ""
     End Sub
+    'Desechos
     Private Sub limpia_desechos()
         cbxModeloDesecho.SelectedIndex = -1
         txtDesechosCantidad.Text = ""
         txtModeloDesecho.Text = ""
     End Sub
+    'Rechazos
+    Private Sub limpia_rechazos()
+        cbxModeloRechazo.SelectedIndex = -1
+        cbxTipoRechazo.SelectedIndex = -1
+        txtRechazosCantidad.Text = ""
+        txtRechazoMotivo.Text = ""
+    End Sub
 #End Region
 #Region "Funciones para modulo Productividad"
+    'Productividad
     Private Sub add_modelo_producido()
         Dim oProduccion As New Produccion
         oProduccion.cve_registro_turno = 1
@@ -240,6 +302,7 @@ Public Class frmProduccion
         oProduccion.estatus = "0"
         oProduccion.elimina_fila_productividad_gridview()
     End Sub
+    'Desechos
     Private Sub add_desecho()
         Dim oDesecho As New Desecho
         oDesecho.cve_registro_turno = 1
@@ -256,5 +319,7 @@ Public Class frmProduccion
     End Sub
 #End Region
 
+
+    
 
 End Class
