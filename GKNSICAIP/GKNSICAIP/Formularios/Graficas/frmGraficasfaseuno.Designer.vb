0162c1a1613df72625d4d0fd42102c7ec296ff24
Oscarz90 <omsz90@live.com.mx>
2013-08-28 11:24:34 -0500

    cambiar y hacer nuevo formulario graficas

---
+++ GKNSICAIP/GKNSICAIP/Formularios/Graficas/frmGraficasfaseuno.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmGraficasfaseuno
+    Inherits Telerik.WinControls.UI.RadForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RadGroupBox1 = New Telerik.WinControls.UI.RadGroupBox()
+        Me.RadThemeManager1 = New Telerik.WinControls.RadThemeManager()
+        CType(Me.RadGroupBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RadGroupBox1
+        '
+        Me.RadGroupBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping
+        Me.RadGroupBox1.HeaderText = "Seleccion:"
+        Me.RadGroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.RadGroupBox1.Name = "RadGroupBox1"
+        '
+        '
+        '
+        Me.RadGroupBox1.RootElement.Padding = New System.Windows.Forms.Padding(2, 18, 2, 2)
+        Me.RadGroupBox1.Size = New System.Drawing.Size(454, 248)
+        Me.RadGroupBox1.TabIndex = 0
+        Me.RadGroupBox1.Text = "Seleccion:"
+        '
+        'FrmGraficasfaseuno
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1272, 738)
+        Me.Controls.Add(Me.RadGroupBox1)
+        Me.MaximumSize = New System.Drawing.Size(1280, 768)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(1280, 736)
+        Me.Name = "FrmGraficasfaseuno"
+        '
+        '
+        '
+        Me.RootElement.ApplyShapeToControl = True
+        Me.RootElement.MaxSize = New System.Drawing.Size(1280, 768)
+        Me.Text = "Gráficas & Reportes Sicaip"
+        CType(Me.RadGroupBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents RadGroupBox1 As Telerik.WinControls.UI.RadGroupBox
+    Friend WithEvents RadThemeManager1 As Telerik.WinControls.RadThemeManager
+End Class
+
---
+++ GKNSICAIP/GKNSICAIP/Formularios/Graficas/frmGraficasfaseuno.vb
@@ -0,0 +1,3 @@
+﻿Public Class FrmGraficasfaseuno
+
+End Class
