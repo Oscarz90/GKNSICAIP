91e5ebc5558b978e97d522073b47e9e7c3f009ab
paty <clau_pms89@hotmail.com>
2013-10-13 16:24:30 -0500

    save

---
+++ CapaDatos/CapaDatos/CapaDatos/conexiones.vb
@@ -0,0 +1,32 @@
+﻿Public Class conexiones
+    Public Sub New()
+        '"Data Source= Oscar-PC\SQLExpress; initial Catalog=GKNSICAIP; Integrated Security = True"
+        vCadenaSicaip = "Data Source=PATRICIA-PC\SQLEXPRESS2008R2;Integrated Security=SSPI; Password=sistemas; User ID=sa;Initial Catalog=GKNSICAIP_V2"
+        vCadenaKronos = "Data Source=PATRICIA-PC\SQLEXPRESS2008R2;Integrated Security=SSPI; Password=sistemas; User ID=sa;Initial Catalog=wtk52db"
+    End Sub
+
+#Region "Propiedades Cadenas de Conexion de Sicaip y Kronos"
+
+
+    Private vCadenaSicaip As String
+    Public Property CadenaSicaip As String
+        Get
+            Return vCadenaSicaip
+        End Get
+        Set(ByVal value As String)
+            vCadenaSicaip = value
+        End Set
+    End Property
+
+    Private vCadenaKronos As String
+    Public Property CadenaKronos As String
+        Get
+            Return vCadenaKronos
+        End Get
+        Set(ByVal value As String)
+            vCadenaKronos = value
+        End Set
+    End Property
+#End Region
+
+End Class
