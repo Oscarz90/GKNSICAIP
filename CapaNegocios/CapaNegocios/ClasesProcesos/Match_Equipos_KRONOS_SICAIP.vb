140bd105b7ee43101b11e87561b2156a5190354c
ariasjc89 <arias_jc89@hotmail.com>
2014-02-20 14:35:24 -0600

    Creacion Opcion de Match

---
+++ CapaNegocios/CapaNegocios/ClasesProcesos/Match_Equipos_KRONOS_SICAIP.vb
@@ -0,0 +1,143 @@
+﻿Imports CapaDatos
+Imports System.Transactions
+
+Public Class Match_Equipos_KRONOS_SICAIP
+
+    Dim cadena_conexion As New CapaDatos.conexiones
+    Dim oBD As New CapaDatos.CapaDatos(cadena_conexion.CadenaSicaip)
+    Dim oBD_KRONOS As New CapaDatos.CapaDatos(cadena_conexion.CadenaKronos)
+    Dim oEquipo As Equipo
+
+    Dim vCveS_Equipos_En_SICAIP() As String
+
+
+    Public Function Obtener_Equipos_KRONOS() As DataTable
+        Dim vRetorno As DataTable
+        Try
+            vRetorno = oBD_KRONOS.ObtenerTabla("")
+        Catch ex As Exception
+            vRetorno = Nothing
+        End Try
+
+        Return vRetorno
+    End Function
+
+    Public Function Obtener_Equipos_SICAIP() As DataTable
+        Dim vRetorno As DataTable
+        Try
+            vRetorno = oBD.ObtenerTabla("")
+        Catch ex As Exception
+            vRetorno = Nothing
+        End Try
+
+        Return vRetorno
+    End Function
+
+
+    Private Sub Actualizar_Nombre_Equipo(ByVal vCve_Equipo As Integer, ByVal vNombre_Nuevo As String)
+        oEquipo = New Equipo
+        Try
+            oEquipo.Cve_Equipo = oEquipo.Obtener_Id(vCve_Equipo.ToString)
+            oEquipo.Cargar()
+            oEquipo.Equipo = vNombre_Nuevo
+            oEquipo.Registrar()
+        Catch ex As Exception
+
+        End Try      
+    End Sub
+
+    Private Function Nombres_Equipos_Diferentes(ByVal vName_SICAIP As String, ByVal vName_KRONOS As String) As Boolean
+        Dim vRetorno As Boolean = False
+
+        If vName_SICAIP = vName_KRONOS Then
+            vRetorno = False
+        Else
+            vRetorno = True
+        End If
+        Return vRetorno
+    End Function
+
+    Private Sub Obtener_Equipos_Nuevos()
+        Dim vDT_SICAIP As DataTable
+        Dim vDT_KRONOS As DataTable
+
+        Try
+            vDT_SICAIP = Obtener_Equipos_SICAIP()
+            vDT_KRONOS = Obtener_Equipos_KRONOS()
+
+            If vDT_KRONOS.Rows.Count > vDT_SICAIP.Rows.Count Then
+                ''------------------------------------------------------------------------
+                ''-----Mandar mensaje de que se encontro un equipo nuevo
+                ''------------------------------------------------------------------------
+
+            Else
+                ''------------------------------------------------------------------------
+                ''-----Mandar mensaje de que se no se encontro equipos nuevos en KRONOS
+                ''------------------------------------------------------------------------
+            End If
+        Catch ex As Exception
+            MsgBox("Error al obtener Equipos Nuevos")
+        End Try
+    End Sub
+
+    Public Sub Hacer_Match()
+        Dim vDT_SICAIP As DataTable
+        Dim vDT_KRONOS As DataTable
+        Dim vDR_S As DataRow
+        Dim vIndice As Integer = 0
+
+        Using scope As New TransactionScope()
+            Try
+                vDT_SICAIP = Obtener_Equipos_SICAIP()
+                vDT_KRONOS = Obtener_Equipos_KRONOS()
+
+                ''Recorrer Todos los equipos de SICAIP
+                For Each oDR_SICAIP As DataRow In vDT_SICAIP.Rows
+                    vDR_S = oDR_SICAIP
+                    vCveS_Equipos_En_SICAIP.SetValue(vDR_S("cve"), vIndice)
+
+                    For Each oDR_KRONOS As DataRow In vDT_KRONOS.Rows
+                        If vDR_S("cve") = oDR_KRONOS("cve") Then
+                            If Nombres_Equipos_Diferentes(vDR_S("nombre"), oDR_KRONOS("nombre")) = True Then
+
+                                Actualizar_Nombre_Equipo(vDR_S("cve"), oDR_KRONOS("nombre"))
+                                ''------------------------------------------------------------------------
+                                ''-----Mandar mensaje de que se actualizo un equipo en SICAIP
+                                ''------------------------------------------------------------------------
+                                Exit For
+                            End If
+                        End If
+                    Next
+                    vIndice = vIndice + 1
+                Next
+                ''------------------------------------------------------------------------
+                ''-----Mandar mensaje de que se buscaran Equipos Nuevos
+                ''------------------------------------------------------------------------
+                Obtener_Equipos_Nuevos()
+                scope.Complete()
+            Catch ex As Exception
+                MsgBox("Error al hacer Match de Equipos con Kronos")
+            End Try
+        End Using
+    End Sub
+
+    Public Function CrearConsultaKronos_filtrado_Equipos() As String
+        Dim vretorno As String = ""
+
+        If vCveS_Equipos_En_SICAIP.Length > 0 Then
+
+
+
+
+        Else
+            'Consulta sin condiciones
+            vretorno = ""
+        End If
+
+
+
+        Return vretorno
+    End Function
+
+
+End Class
---
+++ GKNSICAIP/GKNSICAIP/Formularios/Catalogos/frmMatch_Equipos_Kronos_SICAIP.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMatch_Equipos_Kronos_SICAIP
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMatch_Equipos_Kronos_SICAIP))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.txtIndicentes = New System.Windows.Forms.TextBox()
+        Me.btnMatch = New Telerik.WinControls.UI.RadButton()
+        Me.btnSalir = New Telerik.WinControls.UI.RadButton()
+        Me.RadLabel1 = New Telerik.WinControls.UI.RadLabel()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnMatch, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnSalir, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Controls.Add(Me.RadLabel1)
+        Me.Panel1.Controls.Add(Me.btnSalir)
+        Me.Panel1.Controls.Add(Me.btnMatch)
+        Me.Panel1.Controls.Add(Me.txtIndicentes)
+        Me.Panel1.Controls.Add(Me.Label11)
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(847, 401)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.BackColor = System.Drawing.Color.Transparent
+        Me.Label11.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.ForeColor = System.Drawing.Color.SteelBlue
+        Me.Label11.Location = New System.Drawing.Point(48, 40)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(419, 32)
+        Me.Label11.TabIndex = 153
+        Me.Label11.Text = "Match Equipos KRONOS-SICAIP"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = Global.GKNSICAIP.My.Resources.Resources.banner_03
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.Location = New System.Drawing.Point(0, 1)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(847, 54)
+        Me.PictureBox2.TabIndex = 152
+        Me.PictureBox2.TabStop = False
+        '
+        'txtIndicentes
+        '
+        Me.txtIndicentes.Location = New System.Drawing.Point(12, 138)
+        Me.txtIndicentes.Multiline = True
+        Me.txtIndicentes.Name = "txtIndicentes"
+        Me.txtIndicentes.Size = New System.Drawing.Size(823, 251)
+        Me.txtIndicentes.TabIndex = 154
+        '
+        'btnMatch
+        '
+        Me.btnMatch.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.btnMatch.Image = CType(resources.GetObject("btnMatch.Image"), System.Drawing.Image)
+        Me.btnMatch.Location = New System.Drawing.Point(564, 91)
+        Me.btnMatch.Name = "btnMatch"
+        Me.btnMatch.Size = New System.Drawing.Size(123, 41)
+        Me.btnMatch.TabIndex = 155
+        Me.btnMatch.Text = "Match"
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.btnSalir.Image = CType(resources.GetObject("btnSalir.Image"), System.Drawing.Image)
+        Me.btnSalir.Location = New System.Drawing.Point(693, 91)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(123, 41)
+        Me.btnSalir.TabIndex = 156
+        Me.btnSalir.Text = "Salir"
+        '
+        'RadLabel1
+        '
+        Me.RadLabel1.Location = New System.Drawing.Point(12, 114)
+        Me.RadLabel1.Name = "RadLabel1"
+        Me.RadLabel1.Size = New System.Drawing.Size(56, 18)
+        Me.RadLabel1.TabIndex = 157
+        Me.RadLabel1.Text = "EVENTOS:"
+        '
+        'frmMatch_Equipos_Kronos_SICAIP
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(847, 401)
+        Me.Controls.Add(Me.Panel1)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(863, 440)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(863, 440)
+        Me.Name = "frmMatch_Equipos_Kronos_SICAIP"
+        Me.Text = "Match Equipos KRONOS-SICAIP"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnMatch, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnSalir, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents txtIndicentes As System.Windows.Forms.TextBox
+    Friend WithEvents RadLabel1 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents btnSalir As Telerik.WinControls.UI.RadButton
+    Friend WithEvents btnMatch As Telerik.WinControls.UI.RadButton
+End Class
