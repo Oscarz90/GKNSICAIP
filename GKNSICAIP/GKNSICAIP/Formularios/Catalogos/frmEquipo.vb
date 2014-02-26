569d6ed58c18c25045cb4010f8ade9ee14ba7665
paty <clau_pms89@hotmail.com>
2014-02-26 16:31:26 -0600

    guardado 26/feb/2014

--- GKNSICAIP/GKNSICAIP/Formularios/Catalogos/frmEquipo.vb
+++ GKNSICAIP/GKNSICAIP/Formularios/Catalogos/frmEquipo.vb
@@ -8,10 +8,10 @@ Public Class frmEquipo
     Dim vDelete_Eliminar As Boolean = True
     Dim vValida_Tlatoani As Integer = 0
     Dim vCveDetalle As Long = 0
-    Dim vT_Tlatoani As Boolean = False
-    Dim vLG As Boolean = False
-    Dim vLET As Boolean = False
-    Dim vImagen As Boolean = False
+    Dim vT_Tlatoani As Boolean = True
+    Dim vLG As Boolean = True
+    Dim vLET As Boolean = True
+    Dim vImagen As Boolean = True
 
     Sub New(Optional ByVal vRegistrar As Boolean = True, Optional ByVal vEliminar As Boolean = True)
         ' Llamada necesaria para el diseñador.
@@ -72,14 +72,13 @@ Public Class frmEquipo
         'Cargar una imagen
         If ofdImagen.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
             Try
-                Validar_Imagen_Correcta()
                 If Validar_Imagen_Correcta() = True Then
                     vImagen = True
                 Else
                     vImagen = False
                 End If
                 Validar_Datos_Completos(vT_Tlatoani, vLG, vLET, vImagen)
-                
+
             Catch ex As Exception
 
             End Try
@@ -87,7 +86,7 @@ Public Class frmEquipo
     End Sub
 
     Private Sub btnModificar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnModificar.Click
-        If txtTipo_Tlatoani.Text <> "" And txtLG.Text <> "" And txtLet.Text <> "" Then
+        If Validar_Datos_Completos(vT_Tlatoani, vLG, vLET, vImagen) = True And Existe_Imagen() = True And txtLet.Text <> "" Then
             If MsgBox("¿Esta seguro de realizar los cambios al equipo?", MsgBoxStyle.YesNo, Me.Text) = MsgBoxResult.Yes Then
                 oEquipo.LETT = txtLet.Text
                 oEquipo.Imagen = oEquipo.ImageToByteArray(pbImagen.Image)
@@ -104,16 +103,18 @@ Public Class frmEquipo
         End If
     End Sub
 
-    Private Sub txtTipo_Tlatoani_KeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtTipo_Tlatoani.KeyUp
+    Private Sub txtLet_KeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtLet.KeyUp
         vValida_Tlatoani = txtTipo_Tlatoani.Text.Count
-
-        If vValida_Tlatoani > 0 Then
-
+        If vValida_Tlatoani > 0 And txtLet.Text <> "" Then
             btnModificar.Enabled = True
+            vLET = True
         Else
+            vLET = False
             btnModificar.Enabled = False
         End If
+        Validar_Datos_Completos(vT_Tlatoani, vLG, vLET, vImagen)
     End Sub
+
 #End Region
 
 #Region "Metodos"
@@ -131,11 +132,8 @@ Public Class frmEquipo
         Me.txtTipo_Tlatoani.DataBindings.Add("Text", oEquipo, "Nombre_Detalle")
         Me.txtLG.DataBindings.Add("Text", oEquipo, "Nombre_Lider")
         Me.txtLet.DataBindings.Add("Text", oEquipo, "LETT")
-        'Me.txtImagen.DataBindings.Add("Text", oEquipo, "Ruta_Imagen")
         Me.txtImagen.Text = ""
         Me.pbImagen.Image = oEquipo.ByteArrayToImage(oEquipo.Imagen)
-
-
     End Sub
 
     Private Sub Controles_Permisos(ByVal vAdd As Boolean, ByVal vDelete As Boolean)
@@ -144,16 +142,10 @@ Public Class frmEquipo
         Else
             Me.btnModificar.Enabled = False
         End If
-
     End Sub
 
     Private Function Validar_Datos_Completos(ByVal vTipo_Tlatoani As Boolean, ByVal vLG As Boolean, ByVal vLET As Boolean, ByVal vImagen As Boolean) As Boolean
         Dim vRetorno As Boolean = False
-        If txtLet.Text <> "" Then
-            vLET = True
-        Else
-            vLET = False
-        End If
         If vTipo_Tlatoani = True And vLG = True And vLET = True And vImagen = True Then
             vRetorno = True
             btnModificar.Enabled = True
@@ -171,7 +163,7 @@ Public Class frmEquipo
         opb.Image = System.Drawing.Image.FromFile(ofdImagen.FileName) 'Asignamos la imagen
         opb.SizeMode = PictureBoxSizeMode.AutoSize 'Dejamos su propiedad en autosize para tener el tamaño original de la misma
         If opb.Size.Width <> 250 Or opb.Size.Height <> 170 Then
-            MsgBox("ERROR, La imagen debe ser de 250px X 170px, FAVOR DE SELECCIONAR OTRA IMAGEN", MsgBoxStyle.Exclamation, "")
+            MsgBox("ERROR, La imagen debe tener un tamaño de de 250px,170px, FAVOR DE SELECCIONAR OTRA IMAGEN", MsgBoxStyle.Exclamation, "")
             vRetorno = False
         Else
             pbImagen.ImageLocation = ofdImagen.FileName
@@ -181,19 +173,17 @@ Public Class frmEquipo
         End If
         Return vRetorno
     End Function
+
+    Private Function Existe_Imagen() As Boolean
+        Dim vRetorno As Boolean = False
+        If pbImagen.Image Is Nothing Then
+            vRetorno = False
+        Else
+            vRetorno = True
+        End If
+        Return vRetorno
+    End Function
+
 #End Region
 
-    'Private Sub txtLet_KeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtLet.KeyUp
-    '    vValida_Tlatoani = txtTipo_Tlatoani.Text.Count
-
-    '    If vValida_Tlatoani > 0 Then
-    '        btnModificar.Enabled = True
-    '        vLET = True
-    '    Else
-    '        vLET = False
-    '        btnModificar.Enabled = False
-    '    End If
-
-    '    Validar_Datos_Completos(vT_Tlatoani, vLG, vLET, vImagen)
-    'End Sub
 End Class
