6ceb2b20e366880e4647625a7d69de983c4cadc9
paty <clau_pms89@hotmail.com>
2014-05-16 09:49:23 -0500

    Agregar tab control 16may

--- GKNSICAIP/GKNSICAIP/Formularios/Graficas/frmGraficasfaseuno.Designer.vb
+++ GKNSICAIP/GKNSICAIP/Formularios/Graficas/frmGraficasfaseuno.Designer.vb
@@ -52,6 +52,9 @@ Partial Class FrmGraficasfaseuno
         Me.lblError = New System.Windows.Forms.Label()
         Me.btnGraficar = New Telerik.WinControls.UI.RadButton()
         Me.picboxIndicador = New System.Windows.Forms.PictureBox()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
         CType(Me.RadGroupBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.RadGroupBox1.SuspendLayout()
         CType(Me.rdbtnGente, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -80,6 +83,7 @@ Partial Class FrmGraficasfaseuno
         Me.radChartView1.SuspendLayout()
         CType(Me.btnGraficar, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picboxIndicador, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
         CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -348,6 +352,7 @@ Partial Class FrmGraficasfaseuno
         CartesianArea1.GridDesign.DrawVerticalFills = False
         CartesianArea1.ShowGrid = True
         Me.radChartView1.AreaDesign = CartesianArea1
+        Me.radChartView1.Controls.Add(Me.TabControl1)
         Me.radChartView1.Controls.Add(Me.lblError)
         Me.radChartView1.Location = New System.Drawing.Point(3, 186)
         Me.radChartView1.Name = "radChartView1"
@@ -390,6 +395,36 @@ Partial Class FrmGraficasfaseuno
         Me.picboxIndicador.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.picboxIndicador.TabIndex = 10
         Me.picboxIndicador.TabStop = False
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(5, 35)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1252, 473)
+        Me.TabControl1.TabIndex = 110
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1244, 447)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(192, 74)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
         '
         'FrmGraficasfaseuno
         '
@@ -444,6 +479,7 @@ Partial Class FrmGraficasfaseuno
         Me.radChartView1.PerformLayout()
         CType(Me.btnGraficar, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.picboxIndicador, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
         CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
@@ -477,5 +513,8 @@ Partial Class FrmGraficasfaseuno
     Friend WithEvents cbxComponente As System.Windows.Forms.ComboBox
     Friend WithEvents lblError As System.Windows.Forms.Label
     Friend WithEvents picboxIndicador As System.Windows.Forms.PictureBox
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
 End Class
 
